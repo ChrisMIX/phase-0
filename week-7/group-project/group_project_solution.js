9503adfa320d04aaacd0b68cee2364ffa9a35c28
Chris Miklius <cmiklius@gmail.com>
2015-11-11 15:41:32 -0600

    add 7.8 file and PERSON 2 PSEUDOCODE

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,13 @@
+/* PSEUDOCODE
+
+Create a FUNCTION sum, that takes as input an array of integers and RETURNS a single integer (the sum of all the numbers). FOR all the given numbers in the array, you must take each one individually and add it to a variable initialized at 0. A counter (separate from the variable in the previous step) will be used to reference the index numbers of the array elements. After an array element is accounted for, the counter is incremented by one. Continue until you reach the end of the array, and then RETURN the number.
+
+
+Create a FUNCTION mean, which takes as input an array of integers and RETURNS a single integer (the average of all the numbers)..
+FOR all the integers in the given array, you will need to pull each one individually and add it to a variable initialized at 0. Use a counter (separate from the aforementioned variable) in order to move through the list. Increment the counter by one after an element is accounted for. Once you have added the numbers up, divide by the LENGTH of the array and RETURN the number.
+
+
+Create a FUNCTION median, which takes as input an array of numbers and RETURNS an integer (the median of the numbers). Take the list of numbers and sort it from least to greatest. If the number of elements in the array is odd, take the CEILING of the result of dividing the array LENGTH by 2, and use that as an index value. Whatever number that yields is your median. If the number of elements is even, take the nth (where n is equal to the array LENGTH divided by 2) and (n+1)th numbers in the array, add them together, and divide by 2. That is your median.
+
+*/
+
